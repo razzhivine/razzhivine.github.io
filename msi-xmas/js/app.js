2516183ebacc19737f88648bec9860887f5195fd
Eugene <razzhivine@gmail.com>
2022-11-28 08:52:54 +0400

    msi-xmas

--- msi-xmas/js/app.js
+++ msi-xmas/js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  
+  $('.hamburger-icon').on('click', function () {
+    $(this).toggleClass('hamburger-icon_close');
+    $('.hamburger-menu').toggleClass('hamburger-menu_active');
+  });
+
+  $('.hamburger-menu__item').on('click', function () {
+    $('.hamburger-icon').removeClass('hamburger-icon_close');
+    $('.hamburger-menu').removeClass('hamburger-menu_active');
+
+    var block = $(this).data('block');
+    
+    $('html, body').animate({
+        scrollTop: $('.block[data-block='+ block +']').offset().top
+    }, 1000); 
+
+  });
 });
